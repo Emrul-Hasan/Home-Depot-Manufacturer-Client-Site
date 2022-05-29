c9408776c8b4aded70a58f1d2640a9f6a98b0146
Emrul-Hasan <imrulhasan003@gmail.com>
2022-05-29 18:14:52 +0600

    404 page add

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import notfound from '../../../images/404.png';
+import notfound from '../../../images/dribbble_1.gif';
 
 
 const NotFound = () => {
@@ -7,7 +7,7 @@ const NotFound = () => {
         <div class="card lg:card-side bg-base-100 px-16">
             <figure><img width={1000} src={notfound} alt="Album" /></figure>
             <div class="flex align-middle px-30">
-                <h2 class="card-title font-bold text-primary">Oops! May be you <span className='text-secondary'>lost</span> your path!!!</h2>
+                <h2 class="card-title font-bold text-primary">Oops! May be you <span className='text-danger'>lost</span> your path!!!</h2>
             </div>
         </div>
     );
